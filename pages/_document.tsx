53d8e5c29d58c646ff1c96483e31c7f7b13a7d49
Ayush Maurya <ayush.maurya1@swiggy.in>
2026-02-01 16:51:38 +0530

    fix: added analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,28 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="https://ik.imagekit.io/ayushprivate/lottie/pFz8XmQjS7.lottie"
+          as="fetch"
+          crossOrigin="anonymous"
+        />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BDF5JLG3EC"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BDF5JLG3EC');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
