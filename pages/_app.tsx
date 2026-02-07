6ab11a650b3b0d489d9a042931038eb9ac0dbfac
Ayush Maurya <ayushmaurya.78@email.com>
2026-02-07 13:16:01 +0530

    fix: added og image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,18 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>My Valentine</title>
         <meta name="description" content="My Valentine" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        <meta property="og:title" content="My Valentine" />
+        <meta property="og:description" content="My Valentine" />
+        <meta property="og:image" content="https://ik.imagekit.io/ayushprivate/lottie/og.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:type" content="website" />
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="My Valentine" />
+        <meta name="twitter:description" content="My Valentine" />
+        <meta name="twitter:image" content="https://ik.imagekit.io/ayushprivate/lottie/og.png" />
       </Head>
       <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Component {...pageProps} />
